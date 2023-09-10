b414bc9f25be9beed1b9d07591a020dd8011dfff
lothringen21 <anerolcherry@gmail.com>
2023-09-09 19:17:50 -0600

    Se modifico el nombre de Contacto a Footer

---
+++ src/Componentes/Footer/Footer.js
@@ -0,0 +1,14 @@
+import "./Contacto.css"
+
+function Contacto (){
+    return (
+        <div>
+            <img src="/img/sakura_banner.png" />
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Contacto
