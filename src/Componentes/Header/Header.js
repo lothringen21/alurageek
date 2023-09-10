fe9979ff429953920ee750c66339851b17e9201e
lothringen21 <anerolcherry@gmail.com>
2023-09-09 19:06:52 -0600

    Se limpia header

--- src/Componentes/Header/Header.js
+++ src/Componentes/Header/Header.js
@@ -10,13 +10,7 @@ function Header (){
         <nav className="navbar">
         <ul className="menu">
           <li><a href="/">Inicio</a></li>
-          <li className="submenu-parent" />
           <li><a href="/articulos">Artículos</a></li>
-            <ul className="submenu">
-            <li><a href="#bouquets">Bouquets</a></li>
-            <li><a href="#centros-mesa">Centros de Mesa</a></li>
-            <li><a href="#floreros">Floreros</a></li>
-          </ul>
           <li><a href="/contacto">Contacto</a></li>
         </ul>
         <div className="login">
