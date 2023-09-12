e1fba191a84d1a75c5e8e5494ad7fe51ac6c70c4
lothringen21 <anerolcherry@gmail.com>
2023-09-11 19:37:12 -0600

    Añadiendo submenu a Header

--- src/Componentes/Header/Header.js
+++ src/Componentes/Header/Header.js
@@ -9,9 +9,18 @@ function Header (){
         </div>
         <nav className="navbar">
         <ul className="menu">
-          <li><a href="/">Inicio</a></li>
-          <li><a href="/articulos">Artículos</a></li>
-          <li><a href="/contacto">Contacto</a></li>
+          <li><a href="#home">Inicio</a></li>
+          <li>
+          <div class="menu-item">
+            <a href="#articulos">Artículos</a>
+            <ul className='submenu'>
+              <li><a href='#bouquets'>Bouquets</a></li>
+              <li><a href='#centros-mesa'>Centros de mesa</a></li>
+              <li><a href='#floreros'>Floreros</a></li>
+            </ul>
+            </div>
+            </li>
+          <li><a href="#contacto">Contacto</a></li>
         </ul>
         <div className="login">
           <button className="login-button">Login</button>
