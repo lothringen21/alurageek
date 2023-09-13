d9553c1c4cea143782cfbd0a820d3498ad373c20
lothringen21 <anerolcherry@gmail.com>
2023-09-13 16:45:48 -0600

    Se crea componente de Copyright

---
+++ src/Componentes/Copyright/Copyright.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Copyright.css';
+
+function Copyright() {
+  return (
+    <footer className="copyright">
+      <p>&copy; 2023 Sakura Florerias | Alurageek. <br /> Todos los derechos reservados. <br /> Creado por Lorena Quevedo López </p>
+    </footer>
+  );
+}
+
+export default Copyright;
