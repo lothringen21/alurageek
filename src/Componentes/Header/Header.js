c62ec414f924256f36999cb4eb1f8c157679503b
lothringen21 <anerolcherry@gmail.com>
2023-09-13 16:39:52 -0600

    Se modifica el submenu

--- src/Componentes/Header/Header.js
+++ src/Componentes/Header/Header.js
@@ -1,33 +1,27 @@
 import React from 'react'
 import './Header.css'
 
-function Header (){
-    return(
-        <header className='header'>
-        <div className='logo'>
-            <img src='./img/sakura_banner.png' alt='banner' />
-        </div>
-        <nav className="navbar">
-        <ul className="menu">
-          <li><a href="#home">Inicio</a></li>
-          <li>
-          <div class="menu-item">
-            <a href="#articulos">Artículos</a>
-            <ul className='submenu'>
-              <li><a href='#bouquets'>Bouquets</a></li>
-              <li><a href='#centros-mesa'>Centros de mesa</a></li>
-              <li><a href='#floreros'>Floreros</a></li>
-            </ul>
-            </div>
-            </li>
-          <li><a href="#contacto">Contacto</a></li>
-        </ul>
-        <div className="login">
-          <button className="login-button">Login</button>
-        </div>
-      </nav>
-        </header>
-    )
-}
 
-export default Header
+export default function Header(){
+  return(
+    <>
+      <header>
+        <img src='/img/sakura_banner.png' alt="logo"></img>
+        <nav className='navbar'>
+          <ul>
+            <li><a href="#inicio">Inicio</a></li>
+            <li><a href="#articulos">Articulos</a>
+              <ul>
+                <li><a href="#bouquets">Bouquets</a></li>
+                <li><a href="#centros-mesa">Centros de mesa</a></li>
+                <li><a href="#floreros">Floreros</a></li>
+              </ul>
+            </li>
+            <li><a href="#contacto">Contacto</a></li>
+            <li><a href="#login"  className='btn-login'>Login & Sign Up</a></li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
