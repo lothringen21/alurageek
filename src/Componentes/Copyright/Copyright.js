394716d1a40512074761e324d5fb3bc70892fa8c
lothringen21 <anerolcherry@gmail.com>
2023-09-13 20:19:37 -0600

    Creando Footer de Copyright

--- src/Componentes/Copyright/Copyright.js
+++ src/Componentes/Copyright/Copyright.js
@@ -4,7 +4,13 @@ import './Copyright.css';
 function Copyright() {
   return (
     <footer className="copyright">
-      <p>&copy; 2023 Sakura Florerias | Alurageek. <br /> Todos los derechos reservados. <br /> Creado por Lorena Quevedo López </p>
+      <h4>&copy; 2023 Sakura Florerias | Alurageek.</h4>
+      <p>Todos los derechos reservados. <br /> Creado por Lorena Quevedo López</p>
+      <div className="links">
+        <a href="https://github.com/lothringen21"><img src="img/LinkSocial/github.png" alt="github" title='GitHub' /></a>
+        <a href="https://www.instagram.com/lorepurr/"><img src="img/LinkSocial/instagram.png" alt="instagram" title='Instagram' /></a>
+        <a href="https://www.linkedin.com/in/lorena-quevedo-l%C3%B3pez-707a5a252/"><img src="img/LinkSocial/linkedin.png" alt="linkedin" title='Linkedin' /></a>
+      </div>
     </footer>
   );
 }
